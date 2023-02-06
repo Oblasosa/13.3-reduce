cddad1810943ef12cc65d98728f06339f0f21d7c
Steven <cow3298@gmail.com>
2023-02-05 17:27:20 -0800

    add to function

--- reduce.js
+++ reduce.js
@@ -6,7 +6,12 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce(function(names, nextObj) {
+        names.push(nextObj[key]);
+        return names;
+    },[]);
+}
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +24,21 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    return str.split('').reduce(function(obj, name) {
+        let lowerCase = name.toLowerCase()
+        if (vowels.indexOf(lowerCase) !== -1) {
+            if (obj[lowerCase]) {
+                obj[lowerCase]++;
+            } else {
+                obj[lowerCase] = 1
+            }
+        }
+    }, {})
+};
+
+console.log(vowelCount('superduperfun'))
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -35,8 +54,13 @@ Examples:
         {title: 'Instructor', name: 'Colt'}
        ]
 */
-
-function addKeyAndValue(arr, key, value) {}
+const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function (acc, next, idx) {
+        acc[idx][key] = value;
+        return acc;
+    }, arr);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
